11c9c06ca44cbf675b726f6e5e854ecfd4f99001
Jere0000 <jeremyfrancescoliiparragui@gmail.com>
2026-04-15 01:48:12 +0000

    feat: add hello world mapping to controller

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -1,11 +1,13 @@
+package com.geeksforgeeks.SpringBootHelloWorld;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hola Mundo desde Ingeniería de Sistemas!";
+    @GetMapping("/")
+    public String helloWorld() {
+        return "<h1>Hello World desde Spring Boot!</h1>";
     }
 }
